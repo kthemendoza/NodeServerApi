ede5699e29a5697b556f70aea2859958745da8ff
Brayan Camargo <brayansti@gmail.com>
2019-04-06 14:41:01 -0500

    utilización de motores

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 const PORT = 3000;
 app.get('/', function (req, res) {
-res.send('Hola mundo desde el sena');
+    // res.send('Hola mundo desde el sena');
+    res.render('index' , {title:'Prueba' , mensaje:'Template Pug'});
 });
 app.listen(3000, function () {
-console.log(`Server running at port ${PORT}`);
+    console.log(`Server running at port ${PORT}`);
 });
+app.set(
+    'view engine' ,'pug',
+)
+app.set(
+    'views', './views'
+)
