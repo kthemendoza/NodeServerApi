53a7c2cbdd9d1089a4e2d4252e56592b4ffb8798
cjropero <cjavieramaya@gmail.com>
2019-04-07 10:44:12 -0500

    Utilización del Middelware

--- server/index.js
+++ server/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var app = express();
 const PORT = 3000;
-app.get('/', function (req, res) {
-res.send('Hola mundo desde el sena');
+
+let estaLogueado = () => false;
+let testMiddelware =  ( (req, res, next) =>{
+  if( estaLogueado() ) {
+    res.send(`!-- USTED FUE AUTORIZADO --!   
+      HTTP =>  ${req.method}   
+      URL =>  ${req.url}
+      IP =>  ${req.ip} 
+      Host =>  ${req.hostname}`
+      );
+    console.log('-VALIDO-');
+    next();
+  }else {
+    res.send(`!X-X NO ESTA LOGUEADO X-X-X DENEGADO X-X!   
+      HTTP =>  ${req.method}   
+      URL =>  ${req.url}
+      IP =>  ${req.ip}
+      Host =>  ${req.hostname}`
+      );
+    console.log('-INVALIDO-');
+  }
 });
-app.listen(3000, function () {
-console.log(`Server running at port ${PORT}`);
-});
+
+
+app.use(testMiddelware);
+
+
+//Esto es pa que no se dañe los otros
+app.get(('/'), (req, res, next) => {
+  res.send(`Servidor corriendo`);
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
