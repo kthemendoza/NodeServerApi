790bcef5a2b982bf8cff80702d0b73b5f2fd324e
david cubides <davidcubides.90@gmail.com>
2019-04-08 20:33:17 -0500

    ejemplo conexion sqlserver y datos quemados

--- server/index.js
+++ server/index.js
@@ -107,3 +107,65 @@ var connection = mysql.createConnection({
 //sconnection.end()
 
 app.use(testMiddelware);
+
+//npm isntall mssql 
+app.get('/sql', function (req, res) {
+   
+  var sql = require("mssql");
+
+  var config = {
+      server: "servidor", // Use your SQL server name
+      database: "BD_DCE", // Database to connect to
+      user: "usuario", // Use your username
+      password: 'password', // Use your password
+      port: 1433,
+      // Since we're on Windows Azure, we need to set the following options
+      options: {
+            encrypt: true
+        }
+     };
+
+// connect to your database
+  sql.connect(config, function (err) {
+  
+      if (err) console.log(err);
+
+      // create Request object
+      var request = new sql.Request();
+         
+      // query to the database and get the records
+      request.query('select * from geoparques', function (err, recordset) {
+          
+          if (err) console.log(err)
+
+          // send records as a response
+          res.send(recordset);
+          
+      });
+  });
+});
+
+
+
+var obtenerTiempo = function (requerimiento, respuesta, siguiente) {
+
+  var text = '{ "employees" : [' +
+                                  '{ "firstName":"John" , "lastName":"Doe" },' +
+                                  '{ "firstName":"Anna" , "lastName":"Smith" },' +
+                                  '{ "firstName":"Peter" , "lastName":"Jones" } ]}'; 
+
+  requerimiento.obtenerTiempo =  text;
+  siguiente();
+};
+
+app.use(obtenerTiempo);
+
+app.get('/escritura', function(requerimiento, respuesta){
+  var textoRespuesta = ' texto de respuesta middelware DCE, LOD ';
+  
+  textoRespuesta += ' solicitud de funcion  ' + requerimiento.obtenerTiempo + ' ';
+  console.log(requerimiento.obtenerTiempo);
+
+  respuesta.send(textoRespuesta);
+})
+
